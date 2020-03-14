8f90ab3c84e8562902a40be244ae34a92f5609c2
bourlito <32640964+bourlito@users.noreply.github.com>
2020-03-14 10:18:48 +0100

    Update Log.java

--- src/com/bourlito/factures/utils/Log.java
+++ src/com/bourlito/factures/utils/Log.java
@@ -3,10 +3,20 @@ package com.bourlito.factures.utils;
 public class Log {
     /**
      * methode d'affiche d'un message dans la console
+     *
+     * @param message contenu
+     */
+    public static void d(Object message) {
+        System.out.println(message.toString());
+    }
+
+    /**
+     * methode d'affiche d'un message dans la console
+     *
      * @param libelle du message
      * @param message contenu
      */
-    public static void d(String libelle, String message){
-        System.out.println(libelle + ": " + message);
+    public static void d(String libelle, Object message) {
+        System.out.println(libelle + ": " + message.toString());
     }
 }
